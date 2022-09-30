85309e97365ce2198901081fdcdacc19c8593fa8
NeerajMalviya1997 <neermalviya1997@gmail.com>
2022-09-30 23:39:06 +0530

    some changes are done

--- Covid_vaccine_app/src/main/java/com/masai/Repo/IdCardDao.java
+++ Covid_vaccine_app/src/main/java/com/masai/Repo/IdCardDao.java
@@ -8,8 +8,10 @@ import com.masai.model.AdharCard;
 import com.masai.model.Appointment;
 import com.masai.model.IDCard;
 import com.masai.model.Pancard;
+
 @Repository
 public interface IdCardDao extends JpaRepository<Appointment, Long> {
-	
-
+//	public IDCard findByPancard(Pancard pancard);
+//
+//	public IDCard findByAdharcard(AdharCard adharcard);
 }
